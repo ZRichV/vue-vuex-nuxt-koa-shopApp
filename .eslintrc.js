ee5a65f4a86fec1856d63dfae90acbf66bcd1bc0
ZRichV <597497946@qq.com>
2020-05-13 21:21:28 +0800

    配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,21 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  // add your custom rules here
-  rules: {
-    'nuxt/no-cjs-in-config': 'off'
-  }
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        '@nuxtjs',
+        'plugin:nuxt/recommended'
+    ],
+    // add your custom rules here
+    rules: {
+        'nuxt/no-cjs-in-config': 'off',
+        'indent': 'off',
+        'eol-last': 0,
+        'space-before-function-paren': 0
+    }
 }
